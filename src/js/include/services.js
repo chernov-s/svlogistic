19a6584b927b33a78ed92f0837abb9f10bed1099
sosed <chernov.net@ya.ru>
2017-04-18 01:09:01 +1000

    Add service list

--- src/js/include/services.js
+++ src/js/include/services.js
@@ -1,9 +1,11 @@
 (function ($) {
 
     var detail = 'services-list__detail',
-        serviceItem = 'services-list__item';
+        serviceItem = 'services-list__item',
+        serviceList = 'services-list';
 
     var $services = $('.' + serviceItem),
+        $serviceList = $('.' + serviceList),
         $detail = $('.' + detail);
 
     var setting = {
@@ -29,47 +31,50 @@
         },
 
         /**
+         * TODO
          * Show/hide children service
          *
          * @param {object} el - parent dom element
          */
         toggleDetail: function (el) {
+            //Очищаем раскрывшийся список услуг
+            $detail.empty();
+
+            //Является ли текущий элемент раскрытым?
             if(el.hasClass(active)) {
                 $services.removeClass(active);
                 $detail.removeClass(active);
-                $detail.hide();
-                $detail.empty();
                 this.toDefault();
+                $detail.hide();
             } else {
-                $detail.show();
-                $detail.empty();
                 $services.removeClass(active);
                 el.addClass(active);
                 $detail.addClass(active);
+                $detail.show();
+                //Копируем дочерние элементы услуги в расрытый элемент detail
                 var child = el.find('.js-child').clone().appendTo('.' + detail);
                 this.toSmall(Math.abs(el.data('row') - 1), child.height());
             }
+
+
         },
 
         /**
          * Change height of item element
          *
-         * @param {number} row
+         * @param {number} row - The line number to which we will assign a class 'small'
          * @param {number} childHeight - height of active js-child
          */
         toSmall: function (row, childHeight) {
             this.toDefault();
-
+            $serviceList.addClass(active);
             $small = $('.' + serviceItem + '[data-row="' + row + '"]');
-
-            var _h = $small.height() - childHeight;
-
-            $small.addClass('small').height(_h);
+            $small.addClass('small');
         },
 
         toDefault: function () {
             $services.removeClass('small');
-            $services.height(setting.defaultHeight);
+            $serviceList.removeClass(active);
         }
     };
 
