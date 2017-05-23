9696870718c2b005a8e503be067e25e93e58f8e2
sosed <chernov.net@ya.ru>
2017-05-24 02:40:17 +1000

    Fix bug. Done service section without animation

--- src/js/include/services.js
+++ src/js/include/services.js
@@ -1,7 +1,7 @@
 (function ($) {
 
     var detail = 'services-list__detail',
-        serviceItem = 'services-list__item',
+        serviceItem = 'js-srv-item',
         serviceList = 'services-list';
 
     var $services = $('.' + serviceItem),
@@ -61,23 +61,57 @@
                 el.addClass(active);
                 $detail.addClass(active);
                 $detail.show();
-                //Копируем дочерние элементы услуги в расрытый элемент detail
+                //Копируем дочерние элементы услуги в раскрытый элемент detail
                 var child = el.find('.js-child').clone().appendTo('.' + detail);
-                this.toSmall(row, child.height());
+
+                if(skel.breakpoint("xsmall").active) {
+
+                    //Сжимаем все элементы, кроме активного
+                    this.compressed( $services );
+                    el.removeClass('compressed');
+
+                    this.sort(el.index());
+                } else {
+                    //Сжимаем строку
+                    this.compressed( $('.' + serviceItem + '[data-row="' + row + '"]') );
+                }
+
             }
 
+
+        },
+
+        /*
+         * Сортировка flex элементов (Используется в мобильной версии)
+         * @param {number} index - номер активного элемента
+         */
+        sort: function (index) {
+            if(index > 1)
+                index -= 1;
+            $services.each(function (i) {
+                $(this).css('order', i * 2);
+                if(index === i) {
+                    $detail.css('order', i * 2 + 1);
+                }
+            });
 
         },
 
         /**
          * Change height of item element
          *
-         * @param {number} row - The line number to which we will assign a class 'small'
-         * @param {number} childHeight - height of active js-child
+         * @param {object} $compressed - The DOM element to which we will assign a class 'small'
          */
-        toSmall: function (row, childHeight) {
+        compressed: function($compressed) {
             this.toDefault();
             $serviceList.addClass(active);
+            $compressed.addClass('compressed');
+        },
+
+
+        toSmall: function (row) {
+
+
             $compressed = $('.' + serviceItem + '[data-row="' + row + '"]');
             $compressed.addClass('compressed');
         },
